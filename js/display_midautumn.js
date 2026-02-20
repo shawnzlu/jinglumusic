8fab5d68788f81c1d46dfa87a141a484bc16d133
shawnzlu <shawnzlu@yahoo.com>
2026-02-20 12:17:49 -0500

    Add files via upload

---
+++ js/display_midautumn.js
@@ -0,0 +1,3 @@
+document.write('We are excited to formally announce that Jane First Mandarin CD Album is on its final stage, and will be released in Toronto soon! This Album has been well prepared, which combine the music talent of Jane Lu and very well known producer and musicians in Canada. To order a copy, please send email to jinglumusic@hotmail.com');
+		
+		
